041e97d8b0a314b674e2e7978280c71f88bec6d2
Fadilix <iamfadile2@gmail.com>
2023-11-05 14:06:28 +0000

    added new characters

--- src/data/data.js
+++ src/data/data.js
@@ -15,24 +15,41 @@ import Broly from "../assets/broly.jpg"
 import Cell from "../assets/cell.jpg"
 import Zeno from "../assets/zeno.webp"
 import Zoro from "../assets/zoro.jpg"
-
+import Vegeto from "../assets/vegeto.webp"
+import MajinVegeta from "../assets/MajinVegeta.jpg"
+import Trunks from "../assets/Trunks.png"
+import GokuSSJ4 from "../assets/gokuSSJ4.jpg"
+import Goten from "../assets/Goten.webp"
+import GokuBlueKaioken from "../assets/GokuBlueKaioken.webp"
+import Gogeta from "../assets/gogeta.png"
+import Gotenks from "../assets/Gotenks.webp"
 
 export const imagesData = [
+
     { name: "Bulma", src: bulma },
     { name: "Chichi", src: chichi },
     { name: "Gohan", src: gohan },
     { name: "Goku", src: goku },
     { name: "Goku UI", src: gokuUI },
-    { name: "Great sayaman", src: gsayaman },
-    { name: "Vegeta", src: vegeta },
+    { name: "Great Sayaman", src: gsayaman },
+    { name: "Vegeta SSJ Blue", src: vegeta },
     { name: "Videl", src: videl },
     { name: "Beerus", src: Beerus },
     { name: "C-17", src: c17 },
     { name: "C-18", src: c18, },
     { name: "Jiren", src: Jiren },
     { name: "Whis", src: Whis },
-    { name: "broly", src: Broly },
+    { name: "Broly", src: Broly },
     { name: "Cell", src: Cell },
     { name: "Zeno", src: Zeno },
-    { name: "Zoro (He got lost again)", src: Zoro }
+    { name: "Zoro (He got lost again)", src: Zoro },
+    { name: "Vegeto", src: Vegeto },
+    { name: "Majin Vegeta", src: MajinVegeta },
+    { name: "Trunks", src: Trunks },
+    { name: "Goku SSJ4", src: GokuSSJ4 },
+    { name: "Goten", src: Goten },
+    { name: "Goku SSJ Blue Kaioken", src: GokuBlueKaioken },
+    { name: "Gogeta", src: Gogeta },
+    { name: "Gotenks", src: Gotenks },
+
 ]
